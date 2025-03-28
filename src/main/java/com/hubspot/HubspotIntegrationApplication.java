0f9d6f7b9c873832dc89dea8481d2aaf19f7cb52
SSantosAlex <lex_ssantos@hotmail.com>
2025-03-28 16:16:06 -0300

    ajustes menores

--- src/main/java/com/hubspot/HubspotIntegrationApplication.java
+++ src/main/java/com/hubspot/HubspotIntegrationApplication.java
@@ -2,8 +2,12 @@ package com.hubspot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableCaching
+@EnableFeignClients(basePackages = "com.hubspot")
 public class HubspotIntegrationApplication {
 
 	public static void main(String[] args) {
